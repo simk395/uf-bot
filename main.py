9a152c82f4a6903329a982d71a98e29978ecf737
Simon Kong <skongnyc@gmail.com>
2022-06-23 13:35:39 -0400

    added loading detection and navigate to ex+

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import keyboard
 def windowEnumerationHandler(hwnd, top_windows):
     top_windows.append((hwnd, win32gui.GetWindowText(hwnd)))
 
-# App is process number; App name is the name of the process
 def getProcess(app_name):
     app = ''
     top_windows = []
@@ -22,36 +21,54 @@ def focusWindow(app):
     win32gui.SetForegroundWindow(app)
 
 def locateButton(btn_name):
-    locate = pyautogui.locateCenterOnScreen('./images/{}_s.png'.format(btn_name), grayscale=False, confidence=0.5)
-    locate = pyautogui.locateCenterOnScreen('./images/{}_m.png'.format(btn_name), grayscale=False, confidence=0.5)
-    locate = pyautogui.locateCenterOnScreen('./images/{}_l.png'.format(btn_name), grayscale=False, confidence=0.5)
-    return locate
-
+    suffix = ["_s", "_m", "_l"]
+    locate = None
+    for i in suffix:
+        locate = pyautogui.locateCenterOnScreen('./images/{}{}.png'.format(btn_name,i), grayscale=False, confidence=0.9)
+        if locate != None:
+            return locate
+    return None
+    
 def clickButton(btn_type):
     try:
         btn = locateButton(btn_type)
         pyautogui.moveTo(btn[0], btn[1])
         pyautogui.click()
+        return True
     except TypeError:
+        print("Could not find button/banner. Returning to Home.")
+        time.sleep(1)
         home_btn = locateButton("home")
         pyautogui.moveTo(home_btn[0], home_btn[1])
         pyautogui.click()
         if home_btn == None:
-            print("Could not find Home Button. Closing App")
+            print("Could not find Home button. Closing App")
+            time.sleep(1)
             quit()
-        return
+        return False
      
-def gotoUniteFight():
-    clickButton("uf")
+def uniteFight():
+    buttons = ["uf", "ufsolo", "ufplus"]
+    success = None
+    for i in buttons:
+        success = clickButton(i)
+        isLoading()
+        if success == False:
+            break
 
 
 def isLoading():
-    return "hello"
-
+    load = locateButton("loading")
+    while load != None:
+        load = locateButton("loading")
+        print("Loading...")
+    return
 
 def main():
     if __name__ == "__main__":
         print("GranBot v1.0.0")
+        print("Playing in Medium Window Size is recommended!")
+        print("Please have Granblue on the Home page when using app.")
         app = getProcess("granblue fantasy")
         if app == '':
             print("Granblue Fantasy is not open. Please open Granblue.")
@@ -60,20 +77,16 @@ def main():
 
         focusWindow(app)
         print("Choice Menu:")
-        print("1. Unite and Fight")
+        print("1. Unite and Fight Extreme+ Farm")
         print("2. Slime Blast")
         print("3. Angel Halo")
         print("4. Event")
-        print(object)
+        print("5. Quit")
         userInput = input()
 
         match userInput:
-            case "1" : return gotoUniteFight()
+            case "1" : return uniteFight()
          
         quit()
-        
-        # # pyautogui.click()
-        # pyautogui.moveTo(location[0], location[1])
-        # pyautogui.click()
     
 main()
